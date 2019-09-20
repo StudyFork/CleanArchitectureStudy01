078a3cc4814dc58e238a103fb38cbe6bb6e28c13
BeokBeok <kekemusa37@nate.com>
2019-09-20 22:17:19 +0900

    BaseActivity 및 Fragment 추가

---
+++ BeokBeok/GithubRepoSearch/app/src/main/java/com/beok/githubreposearch/reposearch/RepoSearchActivity.kt
@@ -0,0 +1,8 @@
+package com.beok.githubreposearch.reposearch
+
+import com.beok.githubreposearch.R
+import com.beok.githubreposearch.base.BaseActivity
+
+class RepoSearchActivity : BaseActivity(
+    R.layout.activity_repo_search
+)
---
+++ BeokBeok/GithubRepoSearch/app/src/main/java/com/beok/githubreposearch/reposearch/RepoSearchFragment.kt
@@ -0,0 +1,11 @@
+package com.beok.githubreposearch.reposearch
+
+
+import com.beok.githubreposearch.R
+import com.beok.githubreposearch.base.BaseFragment
+import com.beok.githubreposearch.databinding.FragmentRepoSearchBinding
+
+class RepoSearchFragment : BaseFragment<FragmentRepoSearchBinding>(
+    R.layout.fragment_repo_search
+)
+
