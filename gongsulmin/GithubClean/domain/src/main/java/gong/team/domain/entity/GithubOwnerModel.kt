557a6c51bde538fde62b4466c76372d08a1a594e
공공공 <tv302@naver.com>
2019-09-21 04:11:05 +0900

    Clean_HW 1 - Domain Model 추가

---
+++ gongsulmin/GithubClean/domain/src/main/java/gong/team/domain/entity/GithubOwnerModel.kt
@@ -0,0 +1,7 @@
+package gong.team.domain.entity
+
+data class GithubOwnerModel (
+    val loginName: String ,
+    val profileUrl: String ,
+    val htmlUrl: String
+)
---
+++ gongsulmin/GithubClean/domain/src/main/java/gong/team/domain/entity/GithubSearchResultModel.kt
@@ -0,0 +1,6 @@
+package gong.team.domain.entity
+
+data class GithubSearchResultModel(
+    val ownerModel: GithubOwnerModel ,
+    val stargazersCount: Int
+)
