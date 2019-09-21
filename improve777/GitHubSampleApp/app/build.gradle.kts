48ddc86a0ed70abe42ca9da06a745d34af9b5d84
Daeyeon Kim <improve777@gmail.com>
2019-09-21 15:40:17 +0900

    HW-1 build.gradle.kts 수정

--- improve777/GitHubSampleApp/app/build.gradle.kts
+++ improve777/GitHubSampleApp/app/build.gradle.kts
@@ -17,6 +17,7 @@ android {
         versionName = Releases.versionName
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
+
     buildTypes {
         getByName("release") {
             isMinifyEnabled = false
@@ -34,11 +35,46 @@ android {
 
 dependencies {
     implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
+    implementation(project(Modules.data))
+    implementation(project(Modules.domain))
+    implementation(project(Modules.common))
+
     implementation(Libraries.kotlin)
     implementation(Libraries.appcompat)
-    implementation(Libraries.ktx)
+    implementation(Libraries.material)
+    implementation(Libraries.ktxCore)
     implementation(Libraries.constraintLayout)
+
+    // test
     testImplementation(Libraries.junit)
     androidTestImplementation(Libraries.androidJunit)
     androidTestImplementation(Libraries.espresso)
+
+    // koin
+    implementation(Libraries.koinCore)
+    implementation(Libraries.koinAndroid)
+    implementation(Libraries.koinViewModel)
+
+    // retrofit
+    implementation(Libraries.retrofit)
+    implementation(Libraries.gsonConverter)
+    implementation(Libraries.rxJavaAdapter)
+    implementation(Libraries.loggingInterceptor)
+
+    // rxJava
+    implementation(Libraries.rxJava)
+    implementation(Libraries.rxKotlin)
+
+    // timber
+    implementation(Libraries.timber)
+
+    // glide
+    implementation(Libraries.glide)
+    kapt(Libraries.glideCompiler)
+
+    // anko
+    implementation(Libraries.anko)
+
+    // tedPermission
+    implementation(Libraries.tedPermission)
 }
