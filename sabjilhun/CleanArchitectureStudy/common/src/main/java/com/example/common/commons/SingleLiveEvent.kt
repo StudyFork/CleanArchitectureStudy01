f5b5961f2db58ca125f536aca70a821df70d44c5
김용훈 <sabgilhun@gmail.com>
2019-09-28 19:04:56 +0900

    커먼 클래스 추가

---
+++ sabjilhun/CleanArchitectureStudy/common/src/main/java/com/example/common/commons/SingleLiveEvent.kt
@@ -0,0 +1,33 @@
+package com.example.common.commons
+
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Observer
+import java.util.concurrent.atomic.AtomicBoolean
+
+class SingleLiveEvent<T> : MutableLiveData<T> {
+
+    constructor() : super()
+
+    constructor(value: T) : super(value)
+
+    private val pendingValue = AtomicBoolean(true)
+
+    override fun setValue(value: T) {
+        pendingValue.set(false)
+        super.setValue(value)
+    }
+
+    override fun postValue(value: T) {
+        pendingValue.set(true)
+        super.setValue(value)
+    }
+
+    override fun observe(owner: LifecycleOwner, observer: Observer<in T>) {
+        super.observe(owner, Observer<T> { t ->
+            if (pendingValue.compareAndSet(true, false)) {
+                observer.onChanged(t)
+            }
+        })
+    }
+}
