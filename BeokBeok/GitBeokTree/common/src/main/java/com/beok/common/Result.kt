32495f30c71a8df07f9ca8c2dd204ed85edfef99
BeokBeok <kekemusa37@nate.com>
2019-10-05 19:57:09 +0900

    Result 클래스 추가

---
+++ BeokBeok/GitBeokTree/common/src/main/java/com/beok/common/Result.kt
@@ -0,0 +1,8 @@
+package com.beok.common
+
+sealed class Result<out R> {
+
+    data class Success<out T>(val data: T) : Result<T>()
+
+    data class Error(val exception: Exception) : Result<Nothing>()
+}
