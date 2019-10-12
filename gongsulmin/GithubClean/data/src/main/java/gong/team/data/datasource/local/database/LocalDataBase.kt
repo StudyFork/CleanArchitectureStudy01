84a1c900b231127b5dc9fdde21ee3a10701e4ee1
Gong <tv302@naver.com>
2019-10-12 21:37:08 +0900

    Clean_HW 2 - room 관련 수정

--- gongsulmin/GithubClean/data/src/main/java/gong/team/data/datasource/local/database/LocalDataBase.kt
+++ gongsulmin/GithubClean/data/src/main/java/gong/team/data/datasource/local/database/LocalDataBase.kt
@@ -6,15 +6,16 @@ import androidx.room.Room
 import androidx.room.RoomDatabase
 import gong.team.data.datasource.local.entity.Token
 
-@Database(entities = [Token::class] , version = 1)
+@Database(entities = [Token::class] , version = 1 , exportSchema = false)
 abstract class LocalDataBase: RoomDatabase() {
     abstract fun TokenDao(): TokenDao
+}
 
+class LocalDataBaseImpl() {
     companion object {
         private var instance: LocalDataBase? = null
         fun getInstance(context: Context): LocalDataBase? {
             if (instance == null) {
-
                 synchronized(LocalDataBase::class) {
                     instance = Room.databaseBuilder(
                         context.applicationContext ,
--- gongsulmin/GithubClean/data/src/main/java/gong/team/data/datasource/local/entity/Token.kt
+++ gongsulmin/GithubClean/data/src/main/java/gong/team/data/datasource/local/entity/Token.kt
@@ -1,8 +1,15 @@
 package gong.team.data.datasource.local.entity
 
+import androidx.room.ColumnInfo
 import androidx.room.Entity
+import androidx.room.PrimaryKey
 
-@Entity
+@Entity(tableName = "token")
 data class Token(
+
+    @PrimaryKey
+    val id: Int ,
+
+    @ColumnInfo(name = "token")
     val token: String
 )
