e0c207b336bbe820a75418baacfe05c00f9e0bf7
BeokBeok <kekemusa37@nate.com>
2019-10-12 22:27:19 +0900

    Repo Browse 관련 레이아웃 추가

--- BeokBeok/GitBeokTree/repoBrowse/src/main/java/com/beok/repobrowse/view/RepoBrowseFragment.kt
+++ BeokBeok/GitBeokTree/repoBrowse/src/main/java/com/beok/repobrowse/view/RepoBrowseFragment.kt
@@ -2,10 +2,14 @@ package com.beok.repobrowse.view
 
 
 import android.os.Bundle
+import androidx.lifecycle.Observer
 import androidx.navigation.fragment.navArgs
 import com.beok.common.base.BaseFragment
+import com.beok.repobrowse.BR
 import com.beok.repobrowse.R
 import com.beok.repobrowse.databinding.FragmentRepoBrowseBinding
+import com.beok.repobrowse.databinding.RvRepoFiletreeItemBinding
+import com.beok.repobrowse.entity.RepoFileTreeResEntity
 import com.beok.repobrowse.viewmodel.RepoBrowseViewModel
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
@@ -18,10 +22,39 @@ class RepoBrowseFragment : BaseFragment<FragmentRepoBrowseBinding, RepoBrowseVie
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         initBinding()
-        showSnackBar(args.repoName)
+        initRecyclerView()
+        setObserve()
+        showContents()
     }
 
     override fun initBinding() {
         binding.vm = viewModel
+    }
+
+    private fun initRecyclerView() {
+        binding.rvFiletree.run {
+            setHasFixedSize(true)
+            adapter = RepoBrowseAdapter<List<RepoFileTreeResEntity>, RvRepoFiletreeItemBinding>(
+                R.layout.rv_repo_filetree_item,
+                BR.repoFileTree,
+                viewModel
+            )
+        }
+    }
+
+    private fun showContents() {
+        viewModel.getRepoFileTree(
+            args.user,
+            args.repoName
+        )
+    }
+
+    private fun setObserve() {
+        viewModel.errMsg.observe(
+            this,
+            Observer {
+                showSnackBar(it.message ?: "")
+            }
+        )
     }
 }
